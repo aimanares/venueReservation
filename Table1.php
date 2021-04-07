bf71cfa16d5b6487c28901959fa453c97116704e
aimanares <61780861+aimanares@users.noreply.github.com>
2021-04-07 09:31:39 +0800

    Add files via upload

---
+++ Table1.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+   
+    <meta charset="utf-8">
+    <title>Table of Conference</title>
+	<link rel="stylesheet" href="homecss.css">
+  </head>
+  <body>
+    
+	
+	  <div class="container">
+	
+	<h3 >Table of Conference</h3>
+	
+	<table  border="1"> <tr><td>Number</td><td>Conference Name</td><td colspan="5"> Time duration</td></tr>
+	
+	<tr><td>1.</td><td>Argentina conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td>
+	</tr>
+	<tr><td>2.</td><td>Belgium conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>3.</td><td>Chile conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>4.</td><td>Denmark conference Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>5.</td><td>Egypt conference Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>6.</td><td>France meeting room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>7.</td><td>Germany conference Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>8.</td><td>Hungary conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>9.</td><td>Ireland conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>10.</td><td>Japan conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>11.</td><td>Kosovo conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>12.</td><td>luxembourg conferece Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>13.</td><td>Malaysia confernce Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>14.</td><td>Norway conferece room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>15.</td><td>Oman conferece Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>16.</td><td>Philippines conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>17.</td><td>Qatar conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>18.</td><td>Russia conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>19.</td><td>Singapore conference Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>20.</td><td>Turkey conference Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>21.</td><td>United Kingdom conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>22.</td><td>Vietnam conference Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>23.</td><td>Western Sahara Meeting room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>24.</td><td>Yemen conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>25.</td><td>Zimbabwe conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>26.</td><td>Australia conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>27.</td><td>Brazil conference Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>28.</td><td>China conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>29.</td><td>Finland conference room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	<tr><td>30.</td><td>Greece conference Room</td><td>8 a.m - 11 a.m </td><td>11 a.m - 2 p.m </td><td>2 p.m - 5 p.m </td><td>5 p.m - 8 p.m </td>
+	<td>8 p.m - 11 p.m </td></tr>
+	</table>
+
+	<form action="homepage.php" method="POST">
+      <div class="button">
+        <input type="submit" name="home" value="Reserve Venue">
+      </div>
+      
+    </form>
+
+   </div>
+    
+    
+  </body>
+</html>
