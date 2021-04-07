351512897c280466d0b8fc308b3783c07df9fb1a
aimanares <61780861+aimanares@users.noreply.github.com>
2021-04-07 09:24:33 +0800

    Update displayData.php

--- displayData.php
+++ displayData.php
@@ -1,4 +1,6 @@
 <html>
+<head>
+</head>
 <body>
 
 <?php
@@ -10,8 +12,7 @@
 
 ?>
 
-
-  <table border = "2" width="900" align="center">
+<table border = "2" width="900" align="center">
   	<tr>
   	<td>No</td>
   	<td>venue</td>
@@ -24,32 +25,41 @@
 
     </tr>
 
-<?php
-  $no=1;
-  foreach($reservations as $rsv ){
-?>
+    <?php
+      $no=1;
+      foreach($reservations as $rsv ){
+    ?>
 
-  <tr>
-  <td><?php echo $no ?></td>;
-  <td><?php echo $rsv['venue'] ?></td>;
-  <td><?php echo $rsv['dateTime']?></td>;
-  <td><?php echo $rsv['name']?></td>;
-  <td><?php echo $rsv['purpose']?></td>;
-  <td><?php echo $rsv['phone']?></td>;
-  <td><?php echo $rsv['email']?></td>;
-  <td><?php echo $rsv['address']?></td>;
-  </tr>
+    <tr>
+    <td><?php echo $no ?></td>;
+    <td><?php echo $rsv['venue'] ?></td>;
+    <td><?php echo $rsv['dateTime']?></td>;
+    <td><?php echo $rsv['name']?></td>;
+    <td><?php echo $rsv['purpose']?></td>;
+    <td><?php echo $rsv['phone']?></td>;
+    <td><?php echo $rsv['email']?></td>;
+    <td><?php echo $rsv['address']?></td>;
+    </tr>
 
-<?php
-  $no=$no+1;
-  }
+    <?php
+      $no=$no+1;
+      }
 
-?>
+    ?>
+    <tr>
+      <td colspan="8" align="center">
+        <form action="homepage.php" method="POST">
+        <div class="button">
+          <input type="submit" name="back" value="Back">
+        </div>
+        </form>
+      </td>
+    
+    </tr>
+    
   </table>
 
-  <ul>
-    <li><a href = "homepage.php">Home</a></li>;
-  </ul>
+
 
 </body>
 </html>
